8170d3597deb7fe16012ba0c92759f1d5d229039
tkris <tkristian@my.wctc.edu>
2023-09-05 08:27:37 -0500

    [1.02]

--- balloons.js
+++ balloons.js
@@ -1,4 +1,16 @@
 $(function(){
-
     $('#birthday').pickadate({ format: 'mmmm, d' });
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+    // event listener for check/uncheck
+    $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+         $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
+    });
 });
